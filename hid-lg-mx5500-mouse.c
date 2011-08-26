614174ea6c29f8f90e32edeb30a5467ae1374274
Robert Meijers <robert.meijers@gmail.com>
2011-08-26 21:00:10 +0200

    mouse: Add support for getting and setting the scrollmode
    Support getting and setting the scrollmode using the 'scrollmode' file.
    When setting the scrollmode there are a few options. All have to be specified as
    integers and are explained below.
    The first integer determines the mode. The basic modes are, 1, 2, 3 and 4 for
    respectively setting the scrollmode to immediate freespin, immediate
    click-to-click, freespin when the mouse gets moved and click-to-click when the
    mouse is moved. The other modes are 5, for automatic. 7 for switching to one
    of the modes using a button. And 8 for toggling the mode using a single button.
    
    After the number fo the mode, the duration of the setting can be set. 0 for
    temporarily and 1 for default after power on. This setting is optional when
    setting the mode to 1, 2, 3 of 4 and defaults to temporarily.
    
    For the automatic mode, 5, one or two extra options must be specified. If the
    second isn't specified it default to the first. This number determines the
    number of needed clicks a second to switch from click-to-click to freespin.
    The first number specifies the number of 'up' clicks, the second the number of
    'down' clicks.
    
    When setting the mode to two indendend switch buttons, 7, the two button
    numbers have to be specified. The first one sets the button to use to switch to
    freespin and the second the button to switch to click-to-click.
    
    When setting the mode to toggle using a button only one option has to be
    specified. This has to be the number of the toggle button.
    
    Button numbers:
      1: left button (can't be used)
      2: right button (can't be used)
      3: middle (wheel) button
      4: rear thumb button
      5: front thumb button
      6: find button
      7: wheel left tilt
      8: wheel right tilt
      9: side wheel forward
     11: side wheel backward
     13: side wheel pressed

--- hid-lg-mx5500-mouse.c
+++ hid-lg-mx5500-mouse.c
@@ -10,6 +10,8 @@ struct lg_mx5500_mouse {
 	u8 initialized;
 
 	short battery_level;
+	u8 scrollmode_set;
+	u8 scrollmode[3];
 };
 
 struct lg_mx5500_mouse_handler {
@@ -36,6 +38,20 @@ static int lg_mx5500_mouse_request_battery(struct lg_mx5500_mouse *mouse)
 				 mouse->battery_level >= 0);
 }
 
+static int lg_mx5500_mouse_request_scrollmode(struct lg_mx5500_mouse *mouse)
+{
+	u8 cmd[7] = { 0x10, 0x01, LG_MX5500_ACTION_GET, 0x56, 0x00, 0x00, 0x00 };
+
+	if (mouse->scrollmode_set)
+		return 0;
+
+	cmd[1] = mouse->devnum;
+	lg_mx5500_queue_out(mouse->device, cmd, sizeof(cmd));
+
+	return wait_event_interruptible(mouse->received,
+				 mouse->scrollmode_set);
+}
+
 static ssize_t mouse_show_battery(struct device *device,
 			struct device_attribute *attr, char *buf)
 {
@@ -49,8 +65,126 @@ static ssize_t mouse_show_battery(struct device *device,
 
 static DEVICE_ATTR(battery, S_IRUGO, mouse_show_battery, NULL);
 
+static ssize_t mouse_show_scrollmode(struct device *device,
+			struct device_attribute *attr, char *buf)
+{
+	u8 mode;
+	ssize_t length, remaining;
+	char *startbuf;
+	struct lg_mx5500_mouse *mouse = lg_mx5500_mouse_get_from_device(device);
+
+	if (lg_mx5500_mouse_request_scrollmode(mouse))
+		return 0;
+
+	startbuf = buf;
+	mode = mouse->scrollmode[0];
+
+	// msb determines if the setting is temp or default
+	// (0 is temp, 1 is deault)
+	if (mode < 0x80)
+		length = scnprintf(buf, PAGE_SIZE, "temp ");
+	else
+		length = scnprintf(buf, PAGE_SIZE, "default ");
+	remaining = PAGE_SIZE - length;
+	buf += length;
+
+	mode = (mode | 0xF0) - 0xF0;
+	//mode is static
+	if (mode <= 4) {
+		if (mode == LG_MX5500_SCROLLMODE_CLICK_CLICK_MOVE)
+			length += scnprintf(buf, remaining,
+					"click-to-click on move");
+		else if (mode == LG_MX5500_SCROLLMODE_FREESPIN_MOVE)
+			length += scnprintf(buf, remaining,
+					"freespin on move");
+		else if (mode == LG_MX5500_SCROLLMODE_CLICK_CLICK_IMMEDIATE)
+			length += scnprintf(buf, remaining,
+					"click-to-click immediate");
+		else if (mode == LG_MX5500_SCROLLMODE_FREESPIN_IMMEDIATE)
+			length += scnprintf(buf, remaining,
+					"freespin immediate");
+	}
+	else if (mode == LG_MX5500_SCROLLMODE_AUTOMATIC) {
+		if (mouse->scrollmode[1] == mouse->scrollmode[2])
+			length += scnprintf(buf, remaining,
+					"freespin above %i", mouse->scrollmode[1]);
+		else
+			length += scnprintf(buf, remaining,
+					"freespin above %i up and %i down",
+					mouse->scrollmode[1], mouse->scrollmode[2]);
+	}
+	else if (mode == LG_MX5500_SCROLLMODE_BUTTON_TOGGLE ||
+			(mode == LG_MX5500_SCROLLMODE_BUTTON_SWITCH &&
+			(mouse->scrollmode[1] >> 4) == ((mouse->scrollmode[1] | 0xF0) - 0xF0))) {
+		length += scnprintf(buf, remaining,
+				"toggle using button %i", (mouse->scrollmode[1] | 0xF0) - 0xF0);
+	}
+	else if (mode == LG_MX5500_SCROLLMODE_BUTTON_SWITCH) {
+		length += scnprintf(buf, remaining,
+				"switch to freespin using button %i"
+				" and to click-to-click using button %i",
+				mouse->scrollmode[1] >> 4,
+				(mouse->scrollmode[1] | 0xF0) - 0xF0);
+	}
+
+	if (length < PAGE_SIZE) {
+		startbuf[length] = '\n';
+		length++;
+	}
+
+	return length;
+}
+
+static ssize_t mouse_store_scrollmode(struct device *device,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	short int mode, set_default, first, second, param_count;
+	char cmd[7] = { 0x10, 0x01, LG_MX5500_ACTION_SET, 0x56, 0x00, 0x00, 0x00 };
+	struct lg_mx5500_mouse *mouse = lg_mx5500_mouse_get_from_device(device);
+
+	set_default = 0;
+	param_count = sscanf(buf, "%hi %hi %hi %hi", &mode, &set_default,
+			     &first, &second);
+
+	if (!param_count)
+		return -EINVAL;
+
+	if (mode < 1 || mode == 6 || mode > 8)
+		return -EINVAL;
+
+	cmd[1] = mouse->devnum;
+	cmd[4] = set_default ? mode | 0x80 : mode;
+
+	if (mode == LG_MX5500_SCROLLMODE_AUTOMATIC) {
+		if (param_count < 4)
+			return -EINVAL;
+
+		cmd[5] = first;
+		cmd[6] = second;
+	}
+	else if (mode == LG_MX5500_SCROLLMODE_BUTTON_SWITCH) {
+		if (param_count < 4)
+			return -EINVAL;
+
+		cmd[5] = (first << 4) + second;
+	}
+	else if (mode == LG_MX5500_SCROLLMODE_BUTTON_TOGGLE) {
+		if (param_count < 3)
+			return -EINVAL;
+
+		cmd[6] = first;
+	}
+
+	lg_mx5500_queue_out(mouse->device, cmd, sizeof(cmd));
+
+	return count;
+}
+
+static DEVICE_ATTR(scrollmode, S_IRUGO | S_IWUGO, mouse_show_scrollmode, mouse_store_scrollmode);
+
 static struct attribute *mouse_attrs[] = {
 	&dev_attr_battery.attr,
+	&dev_attr_scrollmode.attr,
 	NULL,
 };
 
@@ -66,9 +200,23 @@ static void mouse_handle_get_battery(
 	mouse->battery_level = buf[4];
 }
 
+static void mouse_handle_scrollmode(
+		struct lg_mx5500_mouse *mouse, const u8 *buf,
+		size_t size)
+{
+	mouse->scrollmode[0] = buf[4];
+	mouse->scrollmode[1] = buf[5];
+	mouse->scrollmode[2] = buf[6];
+	mouse->scrollmode_set = 1;
+}
+
 static struct lg_mx5500_mouse_handler lg_mx5500_mouse_handlers[] = {
 	{ .action = LG_MX5500_ACTION_GET, .first = 0x0d,
 		.func = mouse_handle_get_battery },
+	{ .action = LG_MX5500_ACTION_GET, .first = 0x56,
+		.func = mouse_handle_scrollmode },
+	{ .action = LG_MX5500_ACTION_SET, .first = 0x56,
+		.func = mouse_handle_scrollmode },
 	{ }
 };
 
@@ -109,6 +257,7 @@ struct lg_mx5500_mouse *lg_mx5500_mouse_create(struct lg_mx5500 *device)
 	mouse->device = device;
 	mouse->devnum = 1;
 	mouse->battery_level = -1;
+	mouse->scrollmode_set = 0;
 	mouse->initialized = 0;
 	init_waitqueue_head(&mouse->received);
 
