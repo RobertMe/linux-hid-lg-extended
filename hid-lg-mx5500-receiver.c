b93f96d3fb3372917511cd81061793c82ad63448
Robert Meijers <robert.meijers@gmail.com>
2011-09-21 19:55:30 +0200

    mx5500 receiver: Move struct to code file
    Move the lg_mx5500_receiver struct to the code file as it isn't needed outside of it

--- hid-lg-mx5500-receiver.c
+++ hid-lg-mx5500-receiver.c
@@ -6,6 +6,15 @@
 #include "hid-lg-mx5500-keyboard.h"
 #include "hid-lg-mx5500-receiver.h"
 
+struct lg_mx5500_receiver {
+	u8 initialized;
+	u8 max_devices;
+
+	struct lg_device device;
+
+	struct lg_device *connected_devices[LG_MX5500_RECEIVER_MAX_DEVICES];
+};
+
 int lg_mx5500_receiver_init_new(struct hid_device *hdev);
 
 void lg_mx5500_receiver_exit(struct lg_device *device);
--- hid-lg-mx5500-receiver.h
+++ hid-lg-mx5500-receiver.h
@@ -8,15 +8,6 @@
 
 #define LG_MX5500_RECEIVER_MAX_DEVICES 0x03
 
-struct lg_mx5500_receiver {
-	u8 initialized;
-	u8 max_devices;
-
-	struct lg_device device;
-
-	struct lg_device *connected_devices[LG_MX5500_RECEIVER_MAX_DEVICES];
-};
-
 void lg_mx5500_receiver_exit(struct lg_device *device);
 
 void lg_mx5500_receiver_hid_receive(struct lg_device *device, const u8 *buffer,
