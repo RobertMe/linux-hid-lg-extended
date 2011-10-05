1d2d8a9c671f023b5a92173875c6833b74c46b16
Robert Meijers <robert.meijers@gmail.com>
2011-10-05 21:05:16 +0200

    Add license and copyright notices

--- hid-lg-mx5500-keyboard.h
+++ hid-lg-mx5500-keyboard.h
@@ -1,6 +1,13 @@
 #ifndef __HID_LG_MX5500_KEYBOARD
 #define __HID_LG_MX5500_KEYBOARD
 
+/*
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ */
+
 #include <linux/hid.h>
 
 #include "hid-lg-device.h"
--- hid-lg-mx5500.h
+++ hid-lg-mx5500.h
@@ -1,3 +1,10 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ */
+
 #define USB_DEVICE_ID_MX5500_RECEIVER   0xc71c
 #define USB_DEVICE_ID_MX5500_KEYBOARD   0xb30b
 #define USB_DEVICE_ID_MX5500_MOUSE      0xb007
